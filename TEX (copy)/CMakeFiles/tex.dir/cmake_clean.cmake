bb9568f9c53876247160974003a21453a3d939fe
rsy6318 <rsy6318@163.com>
2017-08-30 14:39:00 +0800

    second upload

---
+++ TEX (copy)/CMakeFiles/tex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tex.dir/tex_generated_main.cu.o"
+  "tex.pdb"
+  "tex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
